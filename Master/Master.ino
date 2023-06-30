99b8f4b8819b0220509fe3fc1d4e13618b8fa3d7
real_batman42 <mikaelschivi@gmail.com>
2023-06-30 18:54:21 -0300

    refatorada de levs

--- Master/Master.ino
+++ Master/Master.ino
@@ -1,96 +1,140 @@
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+/*
+  Codigo responsavel pelo Arduino Interface
+  Componentes que fazem parte:
+  ---------------------------
+  Keypad Matricial 4x4
+  RTC 3231
+  Fingerprint Scanner (?)
+  Buzzer
+  LCD 1602 I2C
+  ---------------------------
+*/
+
+/* incluir bibliotecas */
 #include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
 #include <RTClib.h>
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h> 
 
-char t[32];
+// inicializador RTC
+RTC_DS3231 rtc;
 
-// RTC initializer
-RTC_DS1307 RTC;
-
-// Initializer for Matrix keypad
+// inicializador do Keypad
 const byte ROWS = 4; 
 const byte COLS = 4; 
 
+/* mapeamento do Keypad */
 char hexaKeys[ROWS][COLS] = {
   {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
+
 byte rowPins[ROWS] = {11,10,9,8}; 
 byte colPins[COLS] = {7,6,5,4};
 Keypad keypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
-// Initializer for lcd i2c display
+// inicializador lcd i2c
 LiquidCrystal_I2C lcd(0x27, 16, 2);  
 
 void setup() {
-  Wire.begin();
+  /* inicializacao dos componentes */
   Serial.begin(9600);
-  RTC.begin();
-
-  lcd.backlight();    // turn on lcd blacklight
-  lcd.init();         // turn on lcd
+  while (!Serial){;}
+  Serial.println("Master -> Serial pronto");
   
-  lcd.setCursor(0, 0);                // Initial Setup
-  lcd.print("Insira sua matricula");
-  lcd.setCursor(0, 1);
-  lcd.blink_on();
+  rtc.begin();
+  Wire.begin();
+  lcd.init();
+  lcd.backlight();
+  
+  if (!rtc.begin()) {
+    Serial.println("Master -> RTC 3231 nao encontrado");
+    // while (1);
+  }
+  if (rtc.lostPower()) {                                
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //
+    Serial.println("Master -> RTC DS3231 sync");
+    // textToFile("RTC - Sincronizou", LOG);
+  }
+  
+  lcd.setCursor(5, 0);
+  lcd.print("Aguarde..");
+  delay(1000);
+  lcd.setCursor(5, 1):
+  lcd.print("Ok!");
+  delay(1000);
+  lcd.clear();
 }
-
 
 char id[8];
 int index = 0;
-
-
 void loop() {
+  DateTime now = rtc.now();
   char key = keypad.getKey();
-  
+  lcd.setCursor(0, 0);
+  lcd.print("Matricula: ");
+
   if (key) {
       if( key == '*'){
-      lcd.clear();                // Clear lcd
-      lcd.setCursor(0, 0);        // Set cursor at (0,0) - Top left
-      lcd.print("Bem-vindo :)");  // Print to lcd
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Bem-vindo :)");
       delay(1000);
-      //tone(12, 2500, 125);        // Beep to audible response
 
-      Serial.print('\n');
       Serial.println(id);
 
-      //start the transmission
-      Wire.beginTransmission(9); // Slave Arduino address
-      Wire.write(id);
-      Wire.endTransmission();
-
-      memset(id, 0, sizeof(id));  // Set all elements of id[] to 0 which = NULL
-      index = 0;                  // Set index=0 to remap counter
+      memset(id, 0, sizeof(id));
+      index = 0;
 
       lcd.clear();                
       lcd.setCursor(0, 0);      
-
       lcd.print("Aguarde..");
       delay(2200);
-
-      Serial.print('\n');
                   
       lcd.clear();
-      lcd.setCursor(0, 0);                // Set cursor at (0,0) - Top left
-      lcd.print("Insira sua matricula");  // Print to lcd
-      lcd.blink_on();                     // Cursor blink
-    }
+      lcd.setCursor(0, 0);
+      lcd.print("Insira sua matricula");
+      lcd.blink_on();
+    } else {
+      /*
+        logica pra printa na tela ao mesmo tempo
+        em que a informacao e recebida pelo keypad
+      */
+      id[index] = key;          
+      lcd.setCursor( index, 1 );
+      lcd.print( id[index] );
+      Serial.print( id[index] );
 
-        // Typing routine
-    else{
-          id[index] = key;
-          
-          lcd.setCursor(index, 1);
-          lcd.print(id[index]);
-          Serial.print(id[index]);
-
-          if( index <= sizeof(id) ){
-            index++;
-          }
+      if( index <= sizeof(id) ){
+        index++;
+      }
     }
   }  
+}
+
+/* 
+  simples funcao para abrir e fechar
+  transimissao com o Arduino Slave
+  - pra facilita a vida de Write
+*/
+void SendMessageToSlave( int byte Message, int addr) {
+  Wire.beginTransmission(addr);
+  Wire.write( Message );
+  Wire.endTransmission();
+}
+
+/*
+  facilitar a vida de digitar toda hora
+  lcd.clear ou lcd.setCursor/lcd.print    :d
+*/
+void WriteTextToLcd( int col, int row, char message bool clearDisplay) {
+  if (clearDisplay ){ 
+    lcd.clear();
+  }
+  lcd.setCursor(col, row);
+  lcd.print(message);
 }
