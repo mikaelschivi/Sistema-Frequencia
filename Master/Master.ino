77dc2cb02bf116b53a24cb8d8559402866ce2b2c
real_batman42 <mikaelschivi@gmail.com>
2023-07-04 17:20:53 -0300

    Master/Slave mod

--- Master/Master.ino
+++ Master/Master.ino
@@ -1,119 +1,106 @@
 /*
-  Codigo responsavel pelo Arduino Interface
-  Componentes que fazem parte:
+  MASTER
   ---------------------------
-  Keypad Matricial 4x4
-  RTC 3231
-  Fingerprint Scanner (?)
-  Buzzer
-  LCD 1602 I2C
+  - Keypad Matricial 4x4
+  - Fingerprint Scanner (?)
+  - LCD 1602 I2C
   ---------------------------
 */
 
 /* incluir bibliotecas */
 #include <Keypad.h>
 #include <LiquidCrystal_I2C.h>
-#include <RTClib.h>
-#include <SD.h>
-#include <SPI.h>
-#include <Wire.h> 
-
-// inicializador RTC
-RTC_DS3231 rtc;
+#include <Wire.h>
 
 // inicializador do Keypad
-const byte ROWS = 4; 
-const byte COLS = 4; 
+const byte ROWS = 4;
+const byte COLS = 4;
 
 /* mapeamento do Keypad */
 char hexaKeys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
 };
 
-byte rowPins[ROWS] = {11,10,9,8}; 
-byte colPins[COLS] = {7,6,5,4};
+byte rowPins[ROWS] = { 11, 10, 9, 8 };
+byte colPins[COLS] = { 7, 6, 5, 4 };
 Keypad keypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 // inicializador lcd i2c
-LiquidCrystal_I2C lcd(0x27, 16, 2);  
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   /* inicializacao dos componentes */
   Serial.begin(9600);
-  while (!Serial){;}
+  while (!Serial) {
+    ;
+  }
   Serial.println("Master -> Serial pronto");
-  
-  rtc.begin();
+
   Wire.begin();
   lcd.init();
   lcd.backlight();
-  
-  if (!rtc.begin()) {
-    Serial.println("Master -> RTC 3231 nao encontrado");
-    // while (1);
-  }
-  if (rtc.lostPower()) {                                
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //
-    Serial.println("Master -> RTC DS3231 sync");
-    // textToFile("RTC - Sincronizou", LOG);
-  }
-  
+
+  lcd.clear();
   lcd.setCursor(5, 0);
   lcd.print("Aguarde..");
   delay(1000);
-  lcd.setCursor(5, 1):
-  lcd.print("Ok!");
-  delay(1000);
+  lcd.setCursor(5, 1);
+  lcd.print("Ok !");
+  delay(500);
   lcd.clear();
+
+  Serial.print("MASTER SETUP OK");
 }
 
-char id[8];
+char id[7];
 int index = 0;
 void loop() {
-  DateTime now = rtc.now();
   char key = keypad.getKey();
   lcd.setCursor(0, 0);
   lcd.print("Matricula: ");
 
   if (key) {
-      if( key == '*'){
+    if (key == '*') {
+      SendMessageToSlave(id, 4);
       lcd.clear();
-      lcd.setCursor(0, 0);
+      lcd.setCursor(2, 0);
       lcd.print("Bem-vindo :)");
+
       delay(1000);
 
+
+      Serial.println();
       Serial.println(id);
 
       memset(id, 0, sizeof(id));
       index = 0;
 
-      lcd.clear();                
-      lcd.setCursor(0, 0);      
-      lcd.print("Aguarde..");
-      delay(2200);
-                  
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Insira sua matricula");
+      lcd.print("Aguarde..");
+      delay(2173);
+
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Matricula:");
       lcd.blink_on();
     } else {
       /*
         logica pra printa na tela ao mesmo tempo
         em que a informacao e recebida pelo keypad
       */
-      id[index] = key;          
-      lcd.setCursor( index, 1 );
-      lcd.print( id[index] );
-      Serial.print( id[index] );
+      id[index] = key;
+      WriteTextToLcd(index, 1, id[index], false);
+      Serial.print(id[index]);
 
-      if( index <= sizeof(id) ){
+      if (index <= sizeof(id)) {
         index++;
       }
     }
-  }  
+  }
 }
 
 /* 
@@ -121,18 +108,19 @@ void loop() {
   transimissao com o Arduino Slave
   - pra facilita a vida de Write
 */
-void SendMessageToSlave( int byte Message, int addr) {
+void SendMessageToSlave(char Message[], int addr) {
   Wire.beginTransmission(addr);
-  Wire.write( Message );
+  Wire.write(Message);
   Wire.endTransmission();
+  Serial.println("\nMaster -> Sent data");
 }
 
 /*
   facilitar a vida de digitar toda hora
   lcd.clear ou lcd.setCursor/lcd.print    :d
 */
-void WriteTextToLcd( int col, int row, char message bool clearDisplay) {
-  if (clearDisplay ){ 
+void WriteTextToLcd(int col, int row, char message, bool clearDisplay) {
+  if (clearDisplay) {
     lcd.clear();
   }
   lcd.setCursor(col, row);
--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -1,51 +1,118 @@
+/*
+  SLAVE
+  ---------------------------
+  - Buzzer
+  - SD Module
+  - RTC
+  ---------------------------
+*/
+
 #include <Wire.h>
 #include <RTClib.h>
+#include <SPI.h>
+#include <SD.h>
 
-// // RTC initializer
-//RTC_DS1307 RTC;
+static int CHIPSELECTADDR = 10;
+static int MSADDR = 4;
+static int BUZZER = 4;
 
-// logic for handling information
-typedef struct student{
-  char id[8];
-  int attendances = 0;
-  // fingerprint id
+bool NEWID;
+char eventData[7];
 
-  int day;
-  int month;
-  int hour;
-  int min;
-} student;
-
-student aluno;                  // aluno init
-char id = ' ';
-
+File file;
+RTC_DS3231 rtc;
 
 void setup() {
-  Wire.begin(9); // Slave Arduino address
   Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+
+  // Checagem da inicializacao dos componentes
+  if (!SD.begin(CHIPSELECTADDR)) {
+    Serial.println("--> Error - Inicializacao do SD card falhou!");
+    while (1)
+      ;
+  }
+  if (!SD.exists("data.txt")) {
+    Serial.println("--> Error - Lista de presenca nao encontrada em SD card!");
+    Serial.println("    caso o arquivo nao exista, crie-o!");
+    while (1)
+      ;
+  }
+  Serial.println("SLAVE SETUP OK");
+
+  rtc.begin();
+  delay(10);
+  if (!rtc.begin()) {
+    Serial.println("--> ERROR - Modulo RTC nao encontrado!");
+    while (1)
+      ;
+  }
+  if (rtc.lostPower()) {
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));  //
+    Serial.println("Master -> RTC DS3231 sync");
+    // textToFile("RTC - Sincronizou", LOG);
+  }  
+  
+  pinMode(BUZZER, OUTPUT);
+
+  /*
+    setup conexao I2C com o Arduino Master
+  */
+  Wire.begin(MSADDR);
   Wire.onReceive(receiveEvent);
 }
 
 void loop() {
-  // Slave Arduino loop
-  
+  DateTime now = rtc.now();
+  delay(500);
+
+  if (NEWID == true && eventData != NULL) {
+    file = SD.open("data.txt", FILE_WRITE);
+    if (file) {
+      file.print(eventData);
+      file.print("-");
+      file.print(now.day());
+      file.print("/");
+      file.print(now.month());
+      file.print("-");
+      file.print(now.hour());
+      file.print(":");
+      file.print(now.minute());
+      file.println("\n");
+
+      Serial.print(eventData);
+      Serial.print("-");
+      Serial.print(now.day());
+      Serial.print("/");
+      Serial.print(now.month());
+      Serial.print("-");
+      Serial.print(now.hour());
+      Serial.print(":");
+      Serial.print(now.minute());
+      Serial.println("\n");
+    }
+    file.close();
+    NEWID = false;
+    memset(eventData, NULL, sizeof(eventData));
+
+    delay(100);
+    tone(BUZZER, 1000);  // 1KHz
+    delay(300);          // ... 0.3 seconds
+    noTone(BUZZER);
+  }
 }
 
-int i = 0;
-
-void receiveEvent(int byteCount) {
-  while (Wire.available()) {
-    aluno.id[i] = Wire.read(); 
+/*
+  Funcao base da lib Wire.h
+  recebe event do master
+*/
+void receiveEvent(int howMany) {
+  int i = 0;
+  while (Wire.available() && i < 6) {
+    eventData[i] = Wire.read();
     i++;
   }
-  Serial.print("Slave - id ");
-  Serial.println(aluno.id);
-  // aluno.month = now.month();
-  // Serial.print(now.month());
-  // aluno.day = now.day();
-  // Serial.print(now.day());
-  // aluno.hour = now.hour();
-  // aluno.min = now.minute();
-  // aluno.attendances = aluno.attendances + 1;
-  
+  NEWID = true;
 }
